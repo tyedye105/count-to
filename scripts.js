a059f1f39a319c5da5b423bd011bfb90b27b6b39
Marty Kovach and Michael Poehner <marty@>
2016-10-03 19:40:56 -0700

    intial html, and js with for loop

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("form#two-numbers").submit(function(event) {
+    event.preventDefault();
+    var toCount = parseInt($("input#number-to").val());
+    var byCount = parseInt($("input#number-by").val());
+        alert(toCount + "," + byCount);
+        alert(toCount * byCount);
+    var results = toCount * byCount);
+      for (var index= 0; index <= toCount; index+= byCount {
+        alert(results);
+
+      }
+    });
+
+
+
+  });
